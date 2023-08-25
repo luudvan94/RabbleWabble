f21829115b268cc66b35b5564ce8264483dcc733
luudvan94 <33990794+luudvan94@users.noreply.github.com>
2023-08-25 12:37:45 -0400

    feat: observer

--- RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
+++ RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
@@ -47,6 +47,14 @@ extension SelectQuestionGroupViewController: UITableViewDataSource {
 		withIdentifier: "QuestionGroupCell") as! QuestionGroupCell
 	  let questionGroup = questionGroups[indexPath.row]
 	  cell.titleLabel.text = questionGroup.title
+
+							  cell.percentageSubscriber =
+								questionGroup.score.$runningPercentage // 1
+								  .receive(on: DispatchQueue.main) // 2
+								  .map() { // 3
+									return String(format: "%.0f %%", round(100 * $0))
+								}.assign(to: \.text, on: cell.percentageLabel) // 4
+
 	  return cell
 	}
 
